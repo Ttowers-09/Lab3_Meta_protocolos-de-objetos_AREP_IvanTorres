00fca30d4c59bff4ed9c9cb3bd00202303e2a0f4
Ttowers-09 <ivan.forero-t@mail.escuelaing.edu.co>
2025-08-30 08:38:05 -0500

    registro del GetMapping

--- src/main/java/com/arep/taller1/talle1arep/MicroSpringBoot.java
+++ src/main/java/com/arep/taller1/talle1arep/MicroSpringBoot.java
@@ -10,14 +10,27 @@ public class MicroSpringBoot {
         Class<?> controllerClass = Class.forName(controllerClassName);
         if (controllerClass.isAnnotationPresent(RestController.class)) {
             System.out.println("Controlador: " + controllerClassName);
-            
+            Object controllerInstance = controllerClass.getDeclaredConstructor().newInstance();
             java.lang.reflect.Method[] methods = controllerClass.getDeclaredMethods();
             for (java.lang.reflect.Method method : methods) {
                 if (method.isAnnotationPresent(GetMapping.class)) {
                     GetMapping mapping = method.getAnnotation(GetMapping.class);
-                    System.out.println("Endpoint detectado: " + mapping.value() + method.getName());
+                    String path = mapping.value();
+                    System.out.println("Registrando endpoint: " + path  + method.getName());
+                    
+                    HttpServer.get(path, (req, res) -> {
+                        try {
+                           
+                            Object result = method.invoke(controllerInstance);
+                            return result != null ? result.toString() : "";
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                            return "Error interno";
+                        }
+                    });
                 }
             }
+            HttpServer.main(new String[]{});
         } else {
             System.out.println("No se encuentra");
         }
